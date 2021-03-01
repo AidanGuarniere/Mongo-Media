d72dda36362495ed7cfad7610f4ab7bdbda9bf32
aidan <aidanguarniere@gmail.com>
2021-02-28 17:48:20 -0800

    thought routes and controllers

--- models/Thought.js
+++ models/Thought.js
@@ -1,54 +1,60 @@
 // requirements
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat")
 
-const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: "Please enter a reaction between 1 and 280 characters long!",
-            minLength: 1, 
-            maxLength: 280,
-        },
-        username: {
-            type: String,
-            required: "Please enter your Username!"
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // use getter to format createdAt data before it gets to the controllers
-            get: (createdAtVal) => dateFormat(createdAtVal),
-          },
-    }
-)
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: "Please enter a reaction between 1 and 280 characters long!",
+    minLength: 1,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: "Please enter your Username!",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // use getter to format createdAt data before it gets to the controllers
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+});
 const Reaction = model("reaction", reactionSchema);
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: "Please enter a thought between 1 and 280 characters long!",
-            minLength: 1, 
-            maxLength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // use getter to format createdAt data before it gets to the controllers
-            get: (createdAtVal) => dateFormat(createdAtVal),
-          },
-          reactions: [
-            {
-              type: Schema.Types.ObjectId,
-              ref: "Reaction",
-            },
-          ],
-    }
-)
+  {
+    thoughtText: {
+      type: String,
+      required: "Please enter a thought between 1 and 280 characters long!",
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // use getter to format createdAt data before it gets to the controllers
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    reactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Reaction",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 const Thought = model("Thought", thoughtSchema);
 
-module.exports = {Thought};
+module.exports = { Thought };
